c896a00d70ea1b9fddefd57032907c2d87fa6838
Clearsu <clearwater1219@gmail.com>
2023-09-24 16:23:05 +0900

    feat: gosu, change accessible host

--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -2,14 +2,15 @@
 
 if [ ! -e "/var/lib/mysql/.init_complete" ]; then
   service mysql start
+  sleep 10
 
   mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DB;"
   mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${MYSQL_ROOT_PASSWORD}');"
-  mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-  mysql -e "GRANT ALL ON $MYSQL_DB.* TO '$MYSQL_USER'@'%';"
+  mysql -e "CREATE USER '$MYSQL_USER'@'wordpress' IDENTIFIED BY '$MYSQL_PASSWORD';"
+  mysql -e "GRANT ALL ON $MYSQL_DB.* TO '$MYSQL_USER'@'wordpress';"
   mysql -e "FLUSH PRIVILEGES;"
 
   touch /var/lib/mysql/.init_complete
 fi
 
-exec /usr/sbin/mysqld
+exec gosu mysql /usr/sbin/mysqld --no-daemonize
