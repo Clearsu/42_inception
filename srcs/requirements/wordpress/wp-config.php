fb9c191d39bb97bdfc75e07a44ba5936d4748aba
Clearsu <clearwater1219@gmail.com>
2023-09-24 16:21:39 +0900

    fix: use env variables

--- srcs/requirements/wordpress/wp-config.php
+++ srcs/requirements/wordpress/wp-config.php
@@ -20,10 +20,10 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress_db' );
+define( 'DB_NAME', getenv('MYSQL_DB') );
 
 /** Database username */
-define( 'DB_USER', 'jincpark' );
+define( 'DB_USER', getenv('MYSQL_USER') );
 
 /** Database password */
 define( 'DB_PASSWORD', getenv('MYSQL_PASSWORD') );
